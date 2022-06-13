bd9ada45846c18a5913f0711b9ce64b51ecac93c
Sufyan Shaikh <absufyn@gmail.com>
2022-06-13 16:50:23 +0530

    deploy again

--- hello.py
+++ hello.py
@@ -21,6 +21,7 @@ import pandas as pd
 import pydeck as pdk
 import streamlit as st
 
+
 # SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
 st.set_page_config(layout="wide", page_title="NYC Ridesharing Demo", page_icon=":taxi:")
 
